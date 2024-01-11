ce2c2092c3195d5dc3ffd491281c96b8ed325aec
Privisi <BZhang705@hotmail.com>
2024-01-11 16:32:49 +0900

    Initial Commit

---
+++ src/clojure_training_2024/core.clj
@@ -0,0 +1,7 @@
+(ns clojure-training-2024.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/clojure_training_2024/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-training-2024.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-training-2024.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
