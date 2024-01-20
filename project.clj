64117dba16f7bafdc5daaff216afed4dcbf9adff
Privisi <BZhang705@hotmail.com>
2024-01-20 22:16:05 +0900

    Add live lesson 3

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 ;; JDBC
+                 [org.clojure/java.jdbc "0.7.11"]
+                 [org.xerial/sqlite-jdbc "3.30.1"]]
   :main ^:skip-aot clojure-training-2024.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
