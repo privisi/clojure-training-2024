d78cf54c0a3ee36689d4dc99440b8903b7de8126
Boinciel <boinciel@gmail.com>
2024-01-23 19:57:47 +0900

    Requested fixes for assignment pull request.

--- src/assignments/PAUL_OMAKI/assignment03.clj
+++ src/assignments/PAUL_OMAKI/assignment03.clj
@@ -1,31 +1,28 @@
 (ns assignments.PAUL-OMAKI.assignment03)
 
-;; problem 128
+;; 4clojure problem 128 
 
 
 (defn card-suit-interpreter
   "Accepts standard suit letters (D, H, C, S) of playing card notation and outputs a value that can be interpreted by Clojure. Input should be a single character."
   [char]
-  (let [letter-to-test char]
-    (case letter-to-test
-      \D :diamond    ; If it begins with a D, the suit is diamond, etc.
-      \H :heart
-      \C :club
-      \S :spade
-      "Case not handled yet.")))
+  (case char
+    \D :diamond    ; If it begins with a D, the suit is diamond, etc.
+    \H :heart
+    \C :club
+    \S :spade
+    throw (Exception. "Invalid suit, or no suit supplied to function.")))
 
 (defn card-rank-interpreter
   "Accepts rank letters (A, K, Q, J, T) of playing card notation and outputs a value that can be interpreted by Clojure. Input should be a single character."
   [char]
-  (let [char-to-test char] 
-      (case char-to-test ;; look up case
-        \A 12          ; Named cards and 10.
-        \K 11
-        \Q 10
-        \J 9
-        \T 8 
-        (- (Integer/parseInt (str char-to-test)) 2) ; If input is not one of the above letters, it must be a number.
-        )))
+  (case char ;; look up case
+    \A 12          ; Named cards and 10.
+    \K 11
+    \Q 10
+    \J 9
+    \T 8
+    (- (Integer/parseInt (str char)) 2))) ; If input is not one of the above letters, it must be a number.
 
 (defn card-converter
   "Takes the first and last characters of an input string and attempts to convert it into a map including the card's data.
@@ -37,7 +34,10 @@
 
 
 
-;; problem 178, uses 128 as a base
+;; 4clojure problem 178, uses 128 as a base
+
+
+;; Basic utilities for card checks.
 
 (defn hand-converter
   "Maps card-converter on every card in a given hand."
@@ -57,26 +57,22 @@
 (defn rank-count
   "Returns true if there are [x] cards with the same rank in a given hand."
   [x hand]
-  ;(println (map second (rank-frequencies hand)))
   (some #(= x %) (map second (rank-frequencies hand))))
 
 (defn suit-count
   "Returns true if there are [x] cards with the same suit in a given hand."
   [x hand]
-  ;(println (map second (suit-frequencies hand)))
   (some #(= x %) (map second (suit-frequencies hand))))
 
-(defn two-pair-tester
-  "Tests to see if there are multiple pairs in one hand. Returns true if 2 pairs are found."
-  [hand]
-  (some #(= 2 %) (map second (frequencies (map second (rank-frequencies hand))))))
+
+;; More in-depth functions for specific hands.
 
 (defn card-rank-sorter
   "Puts the ranks of the cards of a given hand in order for further processing."
   [hand]
   (sort (map :rank (hand-converter hand))))
 
-(defn sorted-aces-low-hand 
+(defn sorted-aces-low-hand
   "Sorts hand, then subtracts from the last value of the hand and appends it to the front, simulating an \"aces low\" hand.  
    It doesn't actually matter if the last card is an ace or not, because the next step only looks for a contiguous sequence."
   [hand]
@@ -89,10 +85,33 @@
 
 (defn ascending-rank-checker
   "Checks to see if the ranks of a hand are contiguous or not. Returns true or false."
-  [hand] 
+  [hand]
   (let [sorted-ranks (card-rank-sorter hand)
         sorted-low-ranks (sorted-aces-low-hand hand)]
-     (or (contiguous-number-checker sorted-ranks) (contiguous-number-checker sorted-low-ranks))))
+    (or (contiguous-number-checker sorted-ranks) (contiguous-number-checker sorted-low-ranks))))
+
+
+;; Individual checks for specific hand combinations.
+
+(defn two-pair?
+  "Tests to see if there are multiple pairs in one hand. Returns true if 2 pairs are found."
+  [hand]
+  (some #(= 2 %) (map second (frequencies (map second (rank-frequencies hand))))))
+
+(defn straight-flush?
+  "Tests to see if there are 5 cards of the same suit in hand, and if they are in ascending rank. Returns true if both conditions are fulfilled."
+  [hand]
+  (and (suit-count 5 hand) (ascending-rank-checker hand)))
+
+(defn full-house?
+  "Tests to see if there are multiple sets with the same rank in one hand. Returns true if a set of 3 and a set of 2 are found."
+  [hand]
+  (and (rank-count 3 hand) (rank-count 2 hand)))
+
+(defn straight?
+  "Tests to see if there are 5 cards in ascending rank in hand. Returns true for both aces high and aces low scenarios."
+  [hand]
+  (ascending-rank-checker hand))
 
 
 ;;; The main function of this exercise.
@@ -104,92 +123,12 @@
    \n(poker-hand-categorizer [\"HA\" \"DK\" \"HQ\" \"HJ\" \"HT\"]) => :straight"
   [hand]
   (cond
-    (and (suit-count 5 hand) (ascending-rank-checker hand)) :straight-flush
-    (rank-count 4      hand)      :four-of-a-kind
-    (and (rank-count 3 hand) (rank-count 2 hand))           :full-house
-    (suit-count 5      hand)      :flush
-    (ascending-rank-checker hand) :straight
-    (rank-count 3      hand)      :three-of-a-kind
-    (two-pair-tester   hand)      :two-pair
-    (rank-count 2      hand)      :pair
-    :else                         :high-card))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-;; (defn card-suit-interpreter [placement input]
-;;   (let [char-to-test (placement (str input))] ; get the first character of the input
-;;     (cond
-;;       (= \D char-to-test) :diamond        ;; if it begins with a D, the suit is diamond
-;;       (= \H char-to-test) :heart       
-;;       (= \C char-to-test) :club        
-;;       (= \S char-to-test) :spade       
-;;       (or \A \J \Q \K int) (fn [] card-rank-interpreter)  ;; if 
-;;       :else "Case not handled yet.")))
-
-;; (defn card-rank-interpreter [placement input]
-;;   (let [char-to-test (last (str input))] ; get the first character of the input
-;;     (println char-to-test)
-;;     (cond
-;;       (= \A char-to-test) 12
-;;       (= \K char-to-test) 11
-;;       (= \Q char-to-test) 10
-;;       (= \J char-to-test) 9
-
-;;       (or \D \H \C \S) card-suit-interpreter
-;;       :else "Case not handled yet.")))
-
-;; (defn number-to-rank
-;;   "[x]
-;;    Accepts numbers of standard playing card notation and outputs a value reduced by 2."
-;;   [num]
-;;   (let [num-to-test num]
-;;     (- char-to-test 2)))
-
-;; (defn card-rank-interpreter
-;;   "[x]
-;;    Determines whether the input is a letter or number, then calls the appropriate function to output an integer related to the rank of the card."
-;;   [input]
-;;   (if (int? input)
-;;     (number-to-rank input)
-;;     (letter-to-rank input)))
-
-
-;; These functions were originally split but here's a test of everything integrated
-;; (defn card-suit-rank-interpreter [placement input]
-;;   (let [char-to-test (placement (str input))] ; get one of the characters of the input
-;;     (println char-to-test)
-;;     (cond
-;;       (= \D char-to-test) :diamond    ; if it begins with a D, the suit is diamond, etc.
-;;       (= \H char-to-test) :heart
-;;       (= \C char-to-test) :club
-;;       (= \S char-to-test) :spade
-;;       (= \A char-to-test) 12          ; hardcoding these values
-;;       (= \K char-to-test) 11
-;;       (= \Q char-to-test) 10
-;;       (= \J char-to-test) 9
-;;       (= \T char-to-test) 8
-;;       (int? (int char-to-test)) (- (int char-to-test) 2)
-
-;;       :else "Case not handled yet.")))
-
-;; flawed implementation, it is converting the numbers to string and getting the character value back instead of the raw number
-
-
-
-;; (defn pair-tester [hand]
-;;   (some #(= 2 %) (map second (rank-frequencies hand))))
+    (straight-flush? hand) :straight-flush
+    (rank-count 4    hand) :four-of-a-kind
+    (full-house?     hand) :full-house
+    (suit-count 5    hand) :flush
+    (straight?       hand) :straight
+    (rank-count 3    hand) :three-of-a-kind
+    (two-pair?       hand) :two-pair
+    (rank-count 2    hand) :pair
+    :else                  :high-card))
