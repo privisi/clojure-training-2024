2a459bf6d119948f0d96c8afd1ecc42e51ddf0b1
Boinciel <boinciel@gmail.com>
2024-02-14 11:16:34 +0900

    Leads functionality base

--- src/assignments/PAUL_OMAKI/assignment07.clj
+++ src/assignments/PAUL_OMAKI/assignment07.clj
@@ -9,6 +9,7 @@
 (def base-url "https://paul-sandbox5.pipedrive.com/v1/")
 (def persons-url       (str base-url "persons"))
 (def organizations-url (str base-url "organizations"))
+(def leads-url         (str base-url "leads"))
 (def test-person-params {:name "Test McTesterson"
                          :email "foo@bar.com"
                          :phone "99999999"})
@@ -47,10 +48,20 @@
   []
   (fetch-data organizations-url))
 
+(defn fetch-lead
+  "Creates a future request for a lead's data that matches supplied key and value."
+  [key value]
+  (fetch-data leads-url key value))
+
+(defn fetch-all-leads
+  "Creates a future request for all leads in the database."
+  []
+  (fetch-data leads-url))
+
 
 (defn post-data
   "Generic upload for data. Applies the hardcoded API token."
-  [url & params-map]
+  [url params-map]
   (let [response (client/post url
                                {:as :json
                                 :throw-exceptions false
@@ -66,6 +77,11 @@
   (post-data persons-url params-map))
 
 (defn add-org
+  "Creates a new organization in the database with the supplied map."
+  [params-map]
+  (post-data organizations-url params-map))
+
+(defn add-lead
   "Creates a new organization in the database with the supplied map."
   [params-map]
   (post-data organizations-url params-map))
@@ -88,7 +104,10 @@
   "Gets an organization using the API key and outputs a map with their data."
   [key value]
  (output-entity-data organizations-url key value))
-
+(defn output-lead-data
+  "Gets a person using the API key and outputs a map with their data."
+  [key value]
+  (output-entity-data leads-url key value))
 
 
 (defn strip-person-data
@@ -96,19 +115,40 @@
   [person]
   (let [primary-phone (first (filter #(= (:primary %) true) (:phone person)))
         org-name (get-in person [:org_id :name])]
-    {:name      (:name               person)
+    {:id        (:id                 person)
+     :name      (:name               person)
      :phone     (get primary-phone   :value)
      :email     (:primary_email      person)
      :company   org-name
      :deals     (:closed_deals_count person)}))
+(defn strip-all-persons-data []
+  (map strip-person-data (get (deref (fetch-all-persons)) :data)))
+
 (defn strip-org-data
-  "Reduces the full map JSON to just name, phone, email, company, and their deal count."
+  "Reduces the full map JSON to just name, address, postal code, country, and related deals."
   [org] 
-  {:name                 (:name               org)
+  {:id                   (:id                 org)
+   :name                 (:name               org)
    :address              (:address            org)
    :address_postal_code (:address_postal_code org)
    :address_country      (:address_country    org)
    :deals                (:closed_deals_count org)})
+(defn strip-all-organizations-data []
+  (map strip-org-data (get (deref (fetch-all-orgs)) :data)))
+
+(defn strip-lead-data
+  "Reduces the full map JSON to just name, phone, email, company, and their deal count."
+  [lead]
+  (let [deal-amount (get-in lead [:value :amount])
+        deal-currency (get-in lead [:value :currency])]
+  {:organization_id      (:organization_id      lead)
+   :title                (:title               lead)
+   :amount               deal-amount
+   :currency             deal-currency
+   :expected_close_date  (:expected_close_date lead)
+   :contact              (get (deref (fetch-person :id (get lead :person_id))) :name)}))
+(defn strip-all-organizations-data []
+  (map strip-org-data (get (deref (fetch-all-orgs)) :data)))
 
 
 
